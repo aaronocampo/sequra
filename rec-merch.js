70b43ff60f71ef0d0466189093a8646a65716830
Aaron Ocampo <71071447+aaronocampo@users.noreply.github.com>
2023-04-28 12:40:22 +0100

    Update rec-merch.js

--- rec-merch.js
+++ rec-merch.js
@@ -83,6 +83,14 @@ Barba.Pjax.start();
 
 function setCurrentGroup() {
   var url = window.location.href;
+  var links = document.querySelectorAll('.is--current-group');
+  
+  // remove the class from any other elements that have it
+  for (var i = 0; i < links.length; i++) {
+    links[i].classList.remove("is--current-group");
+  }
+  
+  // add the class to the current group
   if (url.indexOf("recursos") !== -1) {
     document.getElementById("link-recursos").classList.add("is--current-group");
   }
