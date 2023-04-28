024cb8103948dd90eca0464127ac4ffb7a8e689d
Aaron Ocampo <71071447+aaronocampo@users.noreply.github.com>
2023-04-28 13:07:51 +0100

    Back to top update

--- rec-merch.js
+++ rec-merch.js
@@ -42,44 +42,39 @@ Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container
 
 Barba.Pjax.start();
 
-(function($) { "use strict";
+(function($) {
+	"use strict";
 
-	$(document).ready(function(){"use strict";
+	const progressPath = $('.progress-wrap path')[0];
+	const pathLength = progressPath.getTotalLength();
+
+	progressPath.style.transition = 'none';
+	progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+	progressPath.style.strokeDashoffset = pathLength;
+	progressPath.getBoundingClientRect();
+	progressPath.style.transition = 'stroke-dashoffset 10ms linear';
+
+	const updateProgress = () => {
+		const scroll = $(window).scrollTop();
+		const height = $(document).height() - $(window).height();
+		const progress = pathLength - (scroll * pathLength / height);
+		progressPath.style.strokeDashoffset = progress;
 		
-		var progressPath = document.querySelector('.progress-wrap path');
-		var pathLength = progressPath.getTotalLength();
-		progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
-		progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
-		progressPath.style.strokeDashoffset = pathLength;
-		progressPath.getBoundingClientRect();
-		progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
-		var updateProgress = function () {
-			var scroll = $(window).scrollTop();
-			var height = $(document).height() - $(window).height();
-			var progress = pathLength - (scroll * pathLength / height);
-			progressPath.style.strokeDashoffset = progress;
+		if (scroll > 50) {
+			$('.progress-wrap').addClass('active-progress');
+		} else {
+			$('.progress-wrap').removeClass('active-progress');
 		}
-		updateProgress();
-		$(window).scroll(updateProgress);	
-		var offset = 50;
-		var duration = 550;
-		jQuery(window).on('scroll', function() {
-			if (jQuery(this).scrollTop() > offset) {
-				jQuery('.progress-wrap').addClass('active-progress');
-			} else {
-				jQuery('.progress-wrap').removeClass('active-progress');
-			}
-		});				
-		jQuery('.progress-wrap').on('click', function(event) {
-			event.preventDefault();
-			jQuery('html, body').animate({scrollTop: 0}, duration);
-			return false;
-		})
-		
-		
+	};
+
+	$(window).on('scroll', updateProgress);
+	$('.progress-wrap').on('click', function(event) {
+		event.preventDefault();
+		$('html, body').animate({scrollTop: 0}, 550);
+		return false;
 	});
-	
-})(jQuery); 
+})(jQuery);
+
 
 function setCurrentGroup() {
   var url = window.location.href;
