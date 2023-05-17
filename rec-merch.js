b3c37d3714d33f48c1c5897584338f98b49c8ca6
Aaron Ocampo <71071447+aaronocampo@users.noreply.github.com>
2023-05-17 10:26:16 +0100

    Update rec-merch.js

--- rec-merch.js
+++ rec-merch.js
@@ -1,3 +1,29 @@
+function setCurrentGroup() {
+  var url = window.location.href;
+  var links = document.querySelectorAll('.is--current-group');
+  
+  // remove the class from any other elements that have it
+  for (var i = 0; i < links.length; i++) {
+    links[i].classList.remove("is--current-group");
+  }
+  
+  // add the class to the current group
+  if (url.indexOf("recursos") !== -1) {
+    document.getElementById("link-recursos").classList.add("is--current-group");
+  }
+  if (url.indexOf("integracion-visual") !== -1) {
+    document.getElementById("link-integracion").classList.add("is--current-group");
+  }
+  if (url.indexOf("comunicacion") !== -1) {
+    document.getElementById("link-comunicacion").classList.add("is--current-group");
+  }
+  if (url.indexOf("tutorial") !== -1) {
+    document.getElementById("link-tutorial").classList.add("is--current-group");
+  }
+}
+
+setCurrentGroup();
+
 /*  var FadeTransition = Barba.BaseTransition.extend({
   start: function() {
     Promise
@@ -68,17 +94,3 @@ function setCurrentGroup() {
 
 setCurrentGroup();
 */
-
-var url = window.location.href;
-if (url.indexOf("recursos") !== -1) {
-  document.getElementById("link-recursos").classList.add("is--current-group");
-}
-if (url.indexOf("integracion-visual") !== -1) {
-  document.getElementById("link-integracion").classList.add("is--current-group");
-}
-if (url.indexOf("comunicacion") !== -1) {
-  document.getElementById("link-comunicacion").classList.add("is--current-group");
-}
-if (url.indexOf("tutorial") !== -1) {
-  document.getElementById("link-tutorial").classList.add("is--current-group");
-}
