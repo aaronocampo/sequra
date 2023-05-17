5c7e622398481c865260a67197fdb53238480f55
Aaron Ocampo <71071447+aaronocampo@users.noreply.github.com>
2023-05-17 10:42:50 +0100

    Update rec-merch.js

--- rec-merch.js
+++ rec-merch.js
@@ -1,12 +1,16 @@
+window.onload = function() {
+  setCurrentGroup();
+};
+
 function setCurrentGroup() {
   var url = window.location.href;
   var links = document.querySelectorAll('.is--current-group');
-  
+
   // remove the class from any other elements that have it
   for (var i = 0; i < links.length; i++) {
     links[i].classList.remove("is--current-group");
   }
-  
+
   // add the class to the current group
   if (url.indexOf("recursos") !== -1) {
     document.getElementById("link-recursos").classList.add("is--current-group");
@@ -21,8 +25,6 @@ function setCurrentGroup() {
     document.getElementById("link-tutorial").classList.add("is--current-group");
   }
 }
-
-setCurrentGroup();
 
 
 /*  var FadeTransition = Barba.BaseTransition.extend({
